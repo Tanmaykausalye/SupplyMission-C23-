a518612ccdbec106281770872ab4616b4d93e224
Tanmaykausalye <73481990+Tanmaykausalye@users.noreply.github.com>
2020-12-05 20:44:05 +0530

    Add files via upload

---
+++ Redbox.js
@@ -0,0 +1,25 @@
+class Redbox{
+     
+    constructor(x,y,width,height){
+
+  this.body = Bodies.rectangle(x,y,width,height,{isStatic:true})
+  
+  this.height = height
+  this.width = width
+
+  World.add(world,this.body)
+
+
+    }
+
+display(){
+
+var pos = this.body.position
+
+fill("red")
+rect(pos.x,pos.y,this.width,this.height)
+
+}
+
+
+}
